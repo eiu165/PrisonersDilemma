dcb367d5502102168f09c77efe64871c8d162f20
eiu165 <eiu165@gmail.com>
2017-05-26 22:39:12 -0500

    added Bot.Jesus.test

---
+++ src/Bot/Bot.Jesus.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Jesus.Test
+{
+    [TestClass]
+    public class JesusTest
+    {
+        [TestMethod]
+        public void Execute()
+        {
+            Assert.True(true);
+        }
+    }
+}
