e17c3c8f6f8e0fb4d43b196db1318497cdbd4cbe
eiu165 <eiu165@gmail.com>
2017-05-28 23:59:15 -0500

    correct namespace

--- src/Service/Game.cs
+++ src/Service/Game.cs
@@ -1,11 +1,8 @@
-﻿using Domain;
-using System;
+﻿using Domain; 
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq; 
 
-namespace Arena
+namespace Service
 {
 
     public class Game
