9d7ea07ef13a8af86c6e390381509acc4caf3ab0
eiu165 <eiu165@gmail.com>
2017-06-15 23:10:09 -0500

    added NSubstitute

--- src/Service.Test/UnitTest1.cs
+++ src/Service.Test/UnitTest1.cs
@@ -1,5 +1,8 @@
 using System;
 using Xunit;
+using NSubstitute;
+using NSubstitute.Core;
+ 
 
 namespace Service.Test
 {
@@ -13,15 +16,30 @@ namespace Service.Test
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
+
+
 
         int Add(int x, int y)
         {
             return x + y;
         }
+
+        [Fact]
+        public void NSubstituteTest()
+        {
+            var calculator = Substitute.For<ICalculator>();
+            calculator.Add(1, 2).Returns(3);
+            Assert.Equal(3, calculator.Add(1, 2) );
+        }
     }
+
+
+    public interface ICalculator
+    {
+        int Add(int a, int b);
+        string Mode { get; set; }
+        event EventHandler PoweringUp;
+    }
+
+
 }
