9100b60feb1e4d4a56cad78f5ec093625df3abf7
eiu165 <eiu165@gmail.com>
2017-06-15 23:25:01 -0500

    merge from develop

--- src/Service.Test/UnitTest1.cs
+++ src/Service.Test/UnitTest1.cs
@@ -1,14 +1,45 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+using NSubstitute;
+using NSubstitute.Core;
+ 
 
 namespace Service.Test
 {
-    [TestClass]
-    public class UnitTest1
+    
+    
+    public class Class1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void PassingTest()
         {
-            
+            Assert.Equal(4, Add(2, 2));
+        }
+
+
+
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Fact]
+        public void NSubstituteTest()
+        {
+            var calculator = Substitute.For<ICalculator>();
+            calculator.Add(1, 2).Returns(3);
+            Assert.Equal(3, calculator.Add(1, 2) );
         }
     }
+
+
+    public interface ICalculator
+    {
+        int Add(int a, int b);
+        string Mode { get; set; }
+        event EventHandler PoweringUp;
+    }
+
+
 }
--- src/Service/Generation.cs
+++ src/Service/Generation.cs
@@ -37,43 +37,48 @@ namespace Service
 
                 }
             }
+            var playersScore = AddPlayerScores(gameData);
+            Console.WriteLine(" ================================ ");
+            foreach (var playerScore in playersScore)
+            {
+                Console.WriteLine(string.Format(" {0}:{1}  "
+                    , playerScore.Key, playerScore.Value));
+            }
+        }
+
+        public  Dictionary<string, int> AddPlayerScores(List<GameData> gameData)
+        {
             var playersScore = new Dictionary<string, int>();
             foreach (var game in gameData)
             {
-                if (game.Player1Name == "RandomMan"  || game.Player2Name == "RandomMan")
-                Console.WriteLine(string.Format(" {0}:{1} - {2}:{3}"
-                    , game.Player1Name, game.Player1Score
-                    , game.Player2Name, game.Player2Score));
+                if (game.Player1Name == "RandomMan" || game.Player2Name == "RandomMan")
+                    Console.WriteLine(string.Format(" {0}:{1} - {2}:{3}"
+                        , game.Player1Name, game.Player1Score
+                        , game.Player2Name, game.Player2Score));
 
-                var name = game.Player1Name; 
+                var name = game.Player1Name;
                 if (!playersScore.ContainsKey(name))
                 {
-                    playersScore.Add(  name,   game.Player1Score  );
-                }
-                else
-                {
-                    int score = playersScore[name]; 
-                    playersScore[name] = score+ game.Player1Score;
-                }
-
-                name = game.Player2Name; 
-                if (!playersScore.ContainsKey(name))
-                {
-                    playersScore.Add(  name,  game.Player2Score  );
+                    playersScore.Add(name, game.Player1Score);
                 }
                 else
                 {
                     int score = playersScore[name];
-                    playersScore[name] = score+ game.Player1Score;
+                    playersScore[name] = score + game.Player1Score;
                 }
 
+                name = game.Player2Name;
+                if (!playersScore.ContainsKey(name))
+                {
+                    playersScore.Add(name, game.Player2Score);
+                }
+                else
+                {
+                    int score = playersScore[name];
+                    playersScore[name] = score + game.Player1Score;
+                }
             }
-            Console.WriteLine(  " ================================ "  );
-            foreach (var playerScore in playersScore )
-            { 
-                Console.WriteLine(string.Format(" {0}:{1}  "
-                    , playerScore.Key, playerScore.Value));
-            }
+            return playersScore;
         }
     }
 }
